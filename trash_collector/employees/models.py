69a89e426026d98cec4d974c087cf01e8c4b3650
Ted Clayton <ted_clayton@hotmail.com>
2021-11-01 16:36:13 -0400

    Created user profile including categories: name, address, city, state and zip.

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+class Employee_info(models.Model):
+    name = models.CharField(max_length=50)
+    street_address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+
+def __str__(self):
+    return self.name
